234f32fe7093806ed7d7e7b2d1924faeca14b7e3
brun0sk <67489048+brun0sk@users.noreply.github.com>
2020-09-21 21:57:37 -0300

    Modificação
    Para rodar esse gráfico é necessário ter a planilha excel medalhas_olimpiada2016 na mesma pasta em que o gráfico esta guardado.

--- pie_chart.py
+++ pie_chart.py
@@ -1,8 +1,15 @@
 import plotly.graph_objects as go
+import pandas as pd 
+import xlrd
+
+xlsx=pd.ExcelFile('medalhas_olimpiada2016.xlsx')
+df = pd.read_excel(xlsx, 'medalhas_olimpiada2016')
+x=df.loc[df['país']==input("sigla do país:")]
+
 colors = ['#38C976', '#EB5757', '#2D9CDB']
 
 fig = go.Figure(data=[go.Pie(labels=['Ouro', 'Prata', 'Bronze'],
-                             values=[46,37,38])])
+                            values=[float(x['ouro']),float(x['prata']),float(x['bronze'])])])
 fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20,
-                  marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+                marker=dict(colors=colors, line=dict(color='#000000', width=2)))
 fig.show()
