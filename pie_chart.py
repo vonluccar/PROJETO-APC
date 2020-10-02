2874e2061a3bc366f0aea9b110398d65e735f2cc
brun0sk <67489048+brun0sk@users.noreply.github.com>
2020-10-02 19:00:28 -0300

    Update pie_chart.py

--- pie_chart.py
+++ pie_chart.py
@@ -1,8 +1,6 @@
-import plotly.graph_objects as go
+port plotly.graph_objects as go
 import pandas as pd 
 import xlrd
-        marker=dict(colors=colors, line=dict(color='#000000', width=2)))
-fig.show()
 
 xlsx=pd.ExcelFile('medalhas_olimpiada2016.xlsx')
 df = pd.read_excel(xlsx, 'medalhas_olimpiada2016')
@@ -13,3 +11,5 @@ colors = ['#38C976', '#EB5757', '#2D9CDB']
 fig = go.Figure(data=[go.Pie(labels=['Ouro', 'Prata', 'Bronze'],
                             values=[float(x['ouro']),float(x['prata']),float(x['bronze'])])])
 fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20,
+                marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+fig.show()
