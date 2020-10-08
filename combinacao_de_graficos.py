c92604059749d869ac8551bba3204323bbd42b32
jpcg2103 <jpcg2103@gmail.com>
2020-10-08 10:33:04 -0300

    Update combinacao_de_graficos.py
    primeira tentativa

--- combinacao_de_graficos.py
+++ combinacao_de_graficos.py
@@ -1,25 +1,79 @@
-import jupyter
-import plotly.graph_objects as go
-from plotly import colors
+import dash  # use Dash version 1.16.0 or higher for this app to work
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Output, Input
+import plotly.express as px
 
-PIB = ['AUSTRALIA', 'CANADA', 'ESTADOS UNIDOS', 'CHINA', 'JAPÃO','AFRICA DO SUL','ITÁLIA','FRANÇA','INGLATERRA','ALEMANHA','QUÊNIA,',
-                                                                                                                               'RUSSIA','CUBA','ARGENTINA']
+df = px.data.gapminder()
 
-fig = go.Figure(data=[
-    go.Bar(name='2000', x=PIB, y=[0.41, 0.74, 10.25, 1.21, 4.88, 0.13, 1.14, 1.36, 1.65, 1.94, 0.01, 0.25, 0.03, 0.28], marker_color='#FDC132'),
-    go.Bar(name='2004', x=PIB, y=[0.61, 1.02, 12.21, 1.95, 4.81, 0.22, 1.80, 2.11, 2.41, 2.8, 0.01, 0.59, 0.03, 0.16], marker_color='#9B51E0'),
-    go.Bar(name='2008', x=PIB, y=[1.05, 1.54, 14.71, 4.59, 5.03, 0.286, 2.39, 2.91, 2.92, 3.73, 0.03, 1.66, 0.06, 0.36], marker_color='#EB5757'),
-    go.Bar(name='2012', x=PIB, y=[1.54, 1.82, 16.20, 8.53, 6.20, 0.39, 2.08, 2.68, 2.70, 3.52, 0.05, 2.21, 0.07, 0.54], marker_color='#38C976'),
-    go.Bar(name='2016', x=PIB, y=[1.20, 1.52, 18.71, 11.14, 4.93, 0.26, 1.87, 2.47, 2.62, 3.46, 0.06, 1.28, 0.09, 0.55], marker_color='#2D9CDB'),
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Dropdown(id='dpdn2', value=['Germany','Brazil'], multi=True,
+                 options=[{'label': x, 'value': x} for x in
+                          df.country.unique()]),
+    html.Div([
+        dcc.Graph(id='pie-graph', figure={}, className='six columns'),
+        dcc.Graph(id='my-graph', figure={}, clickData=None, hoverData=None, # I assigned None for tutorial purposes. By defualt, these are None, unless you specify otherwise.
+                  config={
+                      'staticPlot': True,     # True, False
+                      'scrollZoom': True,      # True, False
+                      'doubleClick': 'reset',  # 'reset', 'autosize' or 'reset+autosize', False
+                      'showTips': True,       # True, False
+                      'displayModeBar': True,  # True, False, 'hover'
+                      'watermark': True,
+                      # 'modeBarButtonsToRemove': ['pan2d','select2d'],
+                        },
+                  className='six columns'
+                  )
+    ])
 ])
-# Change the bar mode
 
-fig.update_layout(barmode='group')
-fig.show()
 
-labels = ['Brasil','Canada','USA','Áfica do Sul','Itália','Grécia','Japão','China','Austrália','Inglaterra','Alemanha','Quênia','Rússia','Cuba','Argentina','França']
-values = [12, 14, 97, 5, 34, 13, 18, 59, 58, 28, 56, 7, 88, 29, 4, 38]
+@app.callback(
+    Output(component_id='my-graph', component_property='figure'),
+    Input(component_id='dpdn2', component_property='value'),
+)
+def update_graph(data_gapminder):
+    dff = px.data.gapminder()
+    fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
+                     hover_name="country", size="pop",
+                     animation_frame="year",
+                     projection="natural earth")
+    return fig
 
-fig2 = go.Figure(data=[go.Pie(labels=labels, values=values)])
 
-fig.show(fig, fig2)
+# Dash version 1.16.0 or higher
+@app.callback(
+    Output(component_id='pie-graph', component_property='figure'),
+    Input(component_id='my-graph', component_property='hoverData'),
+    Input(component_id='my-graph', component_property='clickData'),
+    Input(component_id='my-graph', component_property='selectedData'),
+    Input(component_id='dpdn2', component_property='value')
+)
+def update_side_graph(hov_data, clk_data, slct_data, country_chosen):
+    if hov_data is None:
+
+        fig2 = px.scatter_geo(df, locations="iso_alpha", color="continent",
+                     hover_name="country", size="pop",
+                     animation_frame="year",
+                     projection="natural earth")
+        return fig2
+    else:
+        print(f'hover data: {hov_data}')
+        # print(hov_data['points'][0]['customdata'][0])
+        # print(f'click data: {clk_data}')
+        # print(f'selected data: {slct_data}')
+        dff2 = df[df.country.isin(country_chosen)]
+        hov_year = hov_data['points'][0]['x']
+        dff2 = dff2[dff2.year == hov_year]
+        fig2 = px.scatter_geo(df, locations="iso_alpha", color="continent",
+                     hover_name="country", size="pop",
+                     animation_frame="year",
+                     projection="natural earth")
+        return fig2
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
