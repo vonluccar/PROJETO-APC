b030100a0eb96afd5ec4e48d267bd70204e50c51
jpanacleto <56097889+jpanacleto2@users.noreply.github.com>
2020-10-20 11:57:30 -0300

    Anacleto fdc

--- gráfico.map.py
+++ gráfico.map.py
@@ -2,6 +2,6 @@ import plotly.express as px
 import pandas as pd
 df=pd.read_csv("mapa.csv",engine='python')
 
-fig = px.scatter_geo(df,animation_frame='ano',locations='iso',color="Pais",hover_name='Pais',size='medalhas',
+fig = px.scatter_geo(df,animation_frame='ano',locations='iso',color="Pais",hover_name='Pais',size='medalhas',size_max=int(40),
                      projection="natural earth",title=str(object=" Medalhas acumuladas por país no mapa"))
 fig.show()  
