b0931b0e958c5b94d230b97e161eaf738650ad4b
vonluccar <lucascarlos60@hotmail.com>
2020-10-26 18:22:02 -0300

    socorro

--- Teste.py
+++ Teste.py
@@ -1,1 +1,70 @@
-print ("hello world")
+import plotly.graph_objects as go
+
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv")
+df.columns = [col.replace("AAPL.", "") for col in df.columns]
+
+# Initialize figure
+fig = go.Figure(go.Bar(
+            x=[20, 14, 23,70],
+            y=['Brasil', 'CUBA', 'Quenia','EUA'],
+            orientation='h'))
+
+# Add Annotations and Buttons
+high_annotations = [dict(x="2016-03-01",
+                         y=df.High.mean(),
+                         xref="x", yref="y",
+                         text="High Average:<br> %.3f" % df.High.mean(),
+                         ax=0, ay=-40),
+                    dict(x=df.High.idxmax(),
+                         y=df.High.max(),
+                         xref="x", yref="y",
+                         text="High Max:<br> %.3f" % df.High.max(),
+                         ax=0, ay=-40)]
+low_annotations = [dict(x="2015-05-01",
+                        y=df.Low.mean(),
+                        xref="x", yref="y",
+                        text="Low Average:<br> %.3f" % df.Low.mean(),
+                        ax=0, ay=40),
+                   dict(x=df.High.idxmin(),
+                        y=df.Low.min(),
+                        xref="x", yref="y",
+                        text="Low Min:<br> %.3f" % df.Low.min(),
+                        ax=0, ay=40)]
+
+fig.update_layout(
+    updatemenus=[
+        dict(
+            active=0,
+            buttons=list([
+                dict(label="None",
+                     method="update",
+                     args=[{"visible": [True, False, True, False]},
+                           {"title": "Yahoo",
+                            "annotations": []}]),
+                dict(label="High",
+                     method="update",
+                     args=[{"visible": [True, True, False, False]},
+                           {"title": "Yahoo High",
+                            "annotations": high_annotations}]),
+                dict(label="Low",
+                     method="update",
+                     args=[{"visible": [False, False, True, True]},
+                           {"title": "Yahoo Low",
+                            "annotations": low_annotations}]),
+                dict(label="Both",
+                     method="update",
+                     args=[{"visible": [True, True, True, True]},
+                           {"title": "Yahoo",
+                            "annotations": high_annotations + low_annotations}]),
+            ]),
+        )
+    ])
+
+# Set title
+fig.update_layout(title_text="Yahoo")
+
+fig.show()
