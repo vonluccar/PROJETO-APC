5313d381b3b4336798df878815b3058b1d08a868
brun0sk <67489048+brun0sk@users.noreply.github.com>
2020-11-23 17:49:56 -0300

    for

--- medalhas_por_país.py
+++ medalhas_por_país.py
@@ -6,49 +6,61 @@ import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input, Output
 
-arquivo = open('pasta1.csv', encoding='utf-8')
-linhas = csv.reader(arquivo)
-x=[]
-for linha in linhas:
-    x.append(linha)
-print(x)
-
+ps=pd.read_csv('pasta1.csv', encoding='UTF-8', sep=';')
 app = dash.Dash(__name__)
+ps.head()
+print(ps.loc[1])
+o=[]
+p=[]
+b=[]
+y=0
+
+for i in ps:
+    o.append(ps.loc[y, 'ouro'])
+    p.append(ps.loc[y, 'prata'])
+    b.append(ps.loc[y, 'bronze'])
+    y+=1
+
+h=0
+for i in o:
+    o[h]=str(o[h])
+    p[h]=str(p[h])
+    b[h]=str(b[h])
+    h+=1
+
+print(o)
 
 app.layout=html.Div([
     html.Label(['Número de Medalhas por Países']),
     dcc.Dropdown(
         id='drop',
         options=[
-                 {'label': 'Ano 2000', 'value': 'Total de Medalhas 2000'},
-                 {'label': 'Ano 2004', 'value': 'Total de Medalhas 2004'},
-                 {'label': 'Ano 2008', 'value': 'Total de Medalhas 2008'},
-                 {'label': 'Ano 2012', 'value': 'Total de Medalhas 2012'},
-                 {'label': 'Ano 2016', 'value': 'Total de Medalhas 2016'},
-        ],
-        value='Total de Medalhas 2000',
+                 {'label': str(ps.loc[i, "países"]), 'value': str(ps.loc[i, ["ouro", "prata", "bronze"]])} for i in range(len(ps))],
+
+        value='ouro',
         multi=False,
         clearable=False,
         style={"width": "50%"}
     ),
-]),
     
-html.Div([
+    html.Div([
     dcc.Graph(id='graph')
 ])
+])
+    
 
 @app.callback(
     Output(component_id='graph', component_property='figure'),
     [Input(component_id='drop', component_property='value')],
-    [Input(component_id='', component_property='value')]
 )
 
 def update_ograf(drop):
-    dff = arquivo
+    dff = ps
 
     piechart=px.pie(
             data_frame=dff,
-            names=my_dropdown,
+            label=drop,
+            value=drop,
             hole=.3,
             )
 
